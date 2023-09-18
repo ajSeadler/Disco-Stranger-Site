f2647a2911e33c605750abc0bdadf93e93df7464
ajSeadler <aj6287@gmail.com>
2023-09-18 09:40:19 -0500

    remove show count

--- script.js
+++ script.js
@@ -5,50 +5,50 @@ document.addEventListener('DOMContentLoaded', function() {
 
   menuIcon.addEventListener('click', function() {
     nav.classList.toggle('active');
-    menuIcon.classList.toggle('active'); // Add or remove "active" class on menu icon
+    menuIcon.classList.toggle('active');
   });
 });
 
-// Function to calculate and display the countdown
-function showCountdown() {
-  // Define the date and time of the target event (September 15, 2023, 7:00 PM)
-  const targetDate = new Date("2023-09-15T19:00:00").getTime();
+//=======COUNTDOWN FUNCTION=================
+// function showCountdown() {
+//   // date is September 15, 2023 7:00 PM
+//   const targetDate = new Date("2023-09-15T20:00:00").getTime();
 
-  // Update the countdown every second
-  const countdownInterval = setInterval(function () {
-    // Get the current date and time
-    const currentDate = new Date().getTime();
+//   // update the countdown every second (interval)
+//   const countdownInterval = setInterval(function () {
+//     // get the current date and time
+//     const currentDate = new Date().getTime();
 
-    // Calculate the time remaining until the target date
-    const timeRemaining = targetDate - currentDate;
+//     // calculate the time remaining until the target date
+//     const timeRemaining = targetDate - currentDate;
 
-    // Calculate days, hours, minutes, and seconds
-    const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+//     // calculate days, hours, minutes, and seconds
+//     const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+//     const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//     const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+//     const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
-    // Display the countdown in the HTML elements
-    document.getElementById("days").textContent = formatTimeValue(days);
-    document.getElementById("hours").textContent = formatTimeValue(hours);
-    document.getElementById("minutes").textContent = formatTimeValue(minutes);
-    document.getElementById("seconds").textContent = formatTimeValue(seconds);
+//     // Display the countdown in the HTML elements
+//     document.getElementById("days").textContent = formatTimeValue(days);
+//     document.getElementById("hours").textContent = formatTimeValue(hours);
+//     document.getElementById("minutes").textContent = formatTimeValue(minutes);
+//     document.getElementById("seconds").textContent = formatTimeValue(seconds);
 
-    // Check if the target date has passed
-    if (timeRemaining < 0) {
-      clearInterval(countdownInterval); // Stop the countdown when the target date is reached
-      document.getElementById("time").textContent = "Event has started!";
-    }
-  }, 1000); // Update every second
-}
+//     // checking if the target date has passed
+//     if (timeRemaining < 0) {
+//       clearInterval(countdownInterval); // stopts the coutdown
+//       document.getElementById("time").textContent = "Event has started!";
+//     }
+//   }, 1000); 
+// }
 
-// Helper function to format time values (add leading zeros)
-function formatTimeValue(value) {
-  return value < 10 ? "0" + value : value;
-}
+// // Helper function to format time values (add leading zeros)
+// function formatTimeValue(value) {
+//   return value < 10 ? "0" + value : value;
+// }
 
-// Call the showCountdown function to start the countdown
-showCountdown();
+// // Call the showCountdown function to start the countdown
+// showCountdown();
 
 
 
